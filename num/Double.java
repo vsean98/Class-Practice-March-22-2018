861b568a4691fd375bd2c8b7d4101b781f99aa2d
Cameron Hyman <cameronhyman@Camerons-MacBook-Air.local>
2018-03-22 14:23:39 -0400

    first commit

---
+++ num/Double.java
@@ -0,0 +1,10 @@
+package num;
+
+public class Double {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ num/String.java
@@ -0,0 +1,19 @@
+package num;
+
+public class String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 String [] Course={ "Calculus","Python","Algebra","Chemisty","science","English","Literature","geography","Orientation","Public Speaking"};
+	        for (int i=0; i<Course.length; i++)
+	        {
+	            System.out.println(Course[i]);
+	        }
+	    }
+
+	
+
+
+	}
+
